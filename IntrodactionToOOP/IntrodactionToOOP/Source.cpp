f615b33f54b87fafd6820ed38de90e1e409f7951
httpc <makshttpc@gmail.com>
2020-05-20 21:28:15 +0300

    1

--- IntrodactionToOOP/IntrodactionToOOP/Source.cpp
+++ IntrodactionToOOP/IntrodactionToOOP/Source.cpp
@@ -3,25 +3,53 @@ using namespace std;
 
 class Point
 {
-public:
+private:
 	double x;
 	double y;
-	Point()
+public:
+	double get_x()
 	{
-
+		return x;
+	}
+	double get_y()
+	{
+		return y;
+	}
+	void set_x(double x)
+	{
+		this->x = x;
+	}
+	void set_y(double y)
+	{
+		this->y = y;
+	}
+};
+class Point3D :public Point
+{
+	double z;
+public:
+	double get_z() const
+	{
+		return z;
+	}
+	void set_z(double z)
+	{
+		this->z = z;
 	}
 };
 
 void main()
 {
-	int a;
+	
 	Point A; 
-	--
-
-	A.x = 2;
-	A.y = 3;
-	cout << A.x << '\t' << A.y << endl;
+	
+	
+	A.set_x(2);
+	A.set_y(4);
+	cout << A.get_x() << '\t' << A.get_y() << endl;
 
 	Point* pA = &A;
-	cout << pA->x << '\t' << A.y << endl;
+	cout << pA->get_x() << '\t' << A.get_y() << endl;
+	Point3D B;
+	B.
 }
